04137cac1c2c3880f42f6fa5d01919f62c1f9ef9
Wesley <wesleyjohnson.stu@gmail.com>
2016-09-08 19:33:25 +0000

    updated jquery links, added two media queries, and created a functional select menu

---
+++ mobile-dropdown/js/app.js
@@ -0,0 +1,27 @@
+//problem: looks gross on small widths
+//solution: dropdown menu
+
+//create select and append to menu
+var $select = $("<select></select>");
+$("#menu").append($select);
+
+//cycle over menu links
+$("#menu a").each(function(){
+    var $anchor = $(this);
+    //create an option
+    var $option = $("<option></option>");
+    //option's value is the href
+    $option.val($anchor.attr("href"));
+    //option's text is the text of the link
+    $option.text($anchor.text());
+    //append option to select
+    $select.append($option);
+});
+//create button    
+var $button = $("<button>Go</button>");
+$("#menu").append($button);
+//bind click to button
+$button.click(function(){
+    //go to select's location
+    window.location = $select.val();
+});
